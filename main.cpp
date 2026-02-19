33b8b3b373192882e6aceaaa0235ed58fbbba8a5
balintmolnar21 <molnar.balint.otto@gmail.com>
2026-02-19 20:26:05 +0100

    Finish

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
     //Elírás, pontos vessző hiánya
     std::cout << "1-100 ertekek duplazasa" << std::endl;
     //Ciklusfeltétel nem teljes
-    for (int i = 0;)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         //így 0-tól kezdjök a számolást, nem 1 től (i+1 kellene)
         b[i] = (i+1) * 2;
